8fe59b63870e3d774389eb52d042cef918453a45
hary077 <mhary9115@gmail.com>
2025-01-14 11:56:50 +0700

    menambahkan proses header

--- auth/.layouts/header.php
+++ auth/.layouts/header.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+  unset($_SESSION ['notification']);
+}
+if (isset($_SESSION["username"]) || isset($_SESSION["role"])){
+  $_SESSION['notification'] = [
+    'type' => 'silahkan logout Terlebih Dahulu!'
+  ];
+  header('locaation: ../dashboard.php');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
