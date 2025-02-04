1b0287db514e7fb3d0cd251dd286c8694ac81010
hary077 <mhary9115@gmail.com>
2025-02-04 13:53:52 +0700

    menambahkan init session(userid)

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,13 +1,21 @@
 <?php
 session_start();
-
+$userId = $_SESSION["user_id"];
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 
 // Ambil notifikasi jika ada, kemudian hapus dari sesi
 $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
+    unset($_SESSION['notification']);
     // Kode untuk menampilkan notifikasi bisa ditambahkan di sini
     // Contoh: echo "<div class='alert alert-info'>$notification</div>";
 }
-unset($_SESSION['notification']);
+if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
+    $_SESSION['notification'] = [
+        'type' => 'danger',
+        'message' => 'Silahkan Login Terlebih Dahulu!'
+    ];
+    header('Location: ./auth/login.php');
+    exit();
+}
